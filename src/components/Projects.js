a50fff03f47625400f78478123cf6625572043a1
DuffKI <kotlyarihor@gmail.com>
2025-03-23 20:28:27 +0200

    Minor Fixes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,17 +3,10 @@ import React from 'react';
 function Projects() {
   return (
     <div className="infobox">
-      <div className="info">
-        <div className="column">
-          <h5>Example project:</h5>
-		  <div class="github-card" data-user="govUA" data-repo="ShapesBlackBoard"></div>
-		  <script src="https://cdn.jsdelivr.net/gh/lepture/github-cards@latest/jsdelivr/widget.js"></script>
-        </div>
-        <div className="column">
-          <h5>Example project:</h5>
-          <a href="https://github.com/govUA/PomodoroTimerApp"><p>Pomodoro Timer App</p></a>
-        </div>
-      </div>
+          <h5>Example projects:</h5>
+		  <a href="https://github.com/govUA/PomodoroTimerApp"><img src="https://gh-card.dev/repos/govUA/PomodoroTimerApp.svg" alt="Card 1"/></a>
+		  <a href="https://github.com/govUA/CLITextProcessor"><img src="https://gh-card.dev/repos/govUA/CLITextProcessor.svg" alt="Card 2"/></a>
+		  <a href="https://github.com/govUA/Unciv"><img src="https://gh-card.dev/repos/govUA/Unciv.svg" alt="Card 3"/></a>
     </div>
   );
 }
